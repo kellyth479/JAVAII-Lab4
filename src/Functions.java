da6a1e8e00763560367ec871d2962cb1a42a1cd7
LiquidGangster <thomasjpkelly@gmail.com>
2018-12-05 00:26:39 -0500

    mergeSort Complete

--- src/Functions.java
+++ src/Functions.java
@@ -1,65 +1,73 @@
+import java.util.Arrays;
+
 public class Functions {
 
     public static void mergeSort(int[] data){
-        if (data.length <= 1){
-            return;
-        }
 
-        int middle = data.length/2;
-        int [] left = new int[middle];
-        int [] right;
-
-        if(data.length % 2 == 0){
-            right = new int[middle];
-        }else{
-            right = new int[middle + 1];
-        }
-
-        for(int i = 0; i < middle; i++){
-            left[i] = data[i];
-        }
-
-        for(int i = 0; i < middle; i++){
-            right[i] = data[middle + i];
-        }
-
-        mergeSort(left);
-        mergeSort(right);
-        merge(left, right);
+        mergeSort(data, 0, data.length - 1);
     }
 
-    public static int binarySearch(int[]data, int key){
-
-
-        return key;
+    public static void mergeSort(int[] data, int beginning, int ending){
+        if(beginning < ending){
+            int middle = (ending + beginning)/2;
+            mergeSort(data, beginning, middle);
+            mergeSort(data, middle + 1, ending);
+            merge(data, beginning, middle, ending);
+        }
     }
+    
+    private static void merge(int[] data, int beginning, int middle, int ending){
 
-    private static int[] merge(int[] left, int[] right){
+        int leftSize = middle - beginning + 1;
+        int rightSize = ending - middle;
 
-        int [] mergedArray = new int[left.length + right.length];
+        int[] left = new int[leftSize];
+        int[] right = new int[rightSize];
+
+        for(int i = 0;i<left.length;i++){
+            left[i] = data[beginning + i];
+        }
+
+        for(int i = 0;i < right.length;i++){
+            right[i] = data[middle + 1 + i];
+        }
 
         int leftTrace = 0;
         int rightTrace = 0;
-        int mergeTrace = 0;
-
-        while(leftTrace < left.length && rightTrace < right.length){
-
-            if(left[leftTrace] <= right[rightTrace]){
-                mergedArray[mergeTrace] = left[leftTrace];
-                mergeTrace =+ mergeTrace;
-                leftTrace =+ leftTrace;
+        int mergeTrace = beginning;
+        //compare left and right array members, add smallest to data
+        while(leftTrace < leftSize && rightTrace < rightSize){
+            if (left[leftTrace] <= right[rightTrace]){
+                data[mergeTrace] = left[leftTrace];
+                leftTrace += 1;
             }else{
-                mergedArray[mergeTrace] = right[rightTrace];
-                mergeTrace =+ mergeTrace;
-                rightTrace =+ rightTrace;
+                data[mergeTrace] = right[rightTrace];
+                rightTrace += 1;
             }
-
-
+            mergeTrace += 1;
         }
+        //move any remainder elements from left to data
+        while(leftTrace < leftSize){
+            data[mergeTrace] = left[leftTrace];
+            leftTrace += 1;
+            mergeTrace += 1;
+        }
+        //move any remainder elements from right to data
+        while(rightTrace < rightSize){
+            data[mergeTrace] = right[rightTrace];
+            rightTrace +=1;
+            mergeTrace += 1;
+        }
+    }
 
+    public static int binarySearch(int[]data, int key){
+        return key;
+    }
 
-
-        return mergedArray;
+    public static void printMe(int[] data){
+        for(int i = 0; i < data.length; i++){
+            System.out.println(data[i]);
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,23 @@ public class Main {
     public static void main(String[] args){
 
         int[] a = {1,3,5,7,9,2,4,6,8};
+        System.out.println("Merging: ");
+        Functions.mergeSort(a);
+        System.out.println("Merged: ");
+        Functions.printMe(a);
 
-//        Functions.mergeSort(a);
+        int[] b = {1};
+        System.out.println("Merging: ");
+        Functions.mergeSort(b);
+        System.out.println("Merged: ");
+        Functions.printMe(b);
+
+        int[] c = {1,1,1,1,4,5,6,7,8,24,24,26,72,2,3,};
+        System.out.println("Merging: ");
+        Functions.mergeSort(c);
+        System.out.println("Merged: ");
+        Functions.printMe(c);
+
 
 //        int idx = Functions.binarySearch(a,4);
 
